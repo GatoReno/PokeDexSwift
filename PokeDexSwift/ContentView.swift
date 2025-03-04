ac105645fbb05a64cc25349831598fd5297b37e2
EdOphanimRawr <erp.reno@hotmail.com>
2025-03-04 12:40:30 -0500

    Initial Commit

---
+++ PokeDexSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeDexSwift
+//
+//  Created by ed on 04/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeDexSwift/PokeDexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexSwiftApp.swift
+//  PokeDexSwift
+//
+//  Created by ed on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
